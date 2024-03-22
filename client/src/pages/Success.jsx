6b22630b0d02aa7369e4f378db90d7d75a30b209
Baivab Kundu <baivabkundu@gmail.com>
2024-03-23 00:45:29 +0530

    success

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import Navbar from "../components/Booking/Navbar";
+import Lottie from "lottie-react";
+import ps from "../payment success animation.json";
 
 const Success = () => {
   return (
     <>
-      <Navbar/>
+      <Navbar />
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-md-6 text-center">
-            <div className="alert alert-success text-center">
-              <h4 className="alert-heading">Payment Successfull</h4>
+            <Lottie
+              animationData={ps}
+              className="m-2"
+              // style={{ width: "300px", height: "1000px" }}
+            ></Lottie>
+             <div className="alert alert-success text-center">
+              <h4 className="alert-heading">Payment Successful</h4>
             </div>
-            <button><a href="/">Back to Home</a></button>
+            <button>
+              <a href="/">Back to Home</a>
+            </button>
           </div>
         </div>
       </div>
